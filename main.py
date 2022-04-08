e2ddc07972eb908095169c743f5225007be34c7f
MarcelFrancke <marcel.francke@unibw.de>
2022-04-08 22:24:40 +0200

    Added S2 and S3 Constraint

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from elements.Nurse import Nurse
 #import pandas as pd
 from elements.Shift import Shift
 from ortools.sat.python import cp_model
+from itertools import product
+
 
 
 if __name__ == '__main__':
@@ -12,7 +14,6 @@ if __name__ == '__main__':
     shiftTypes = []
     shiftRepr = ['O', 'E', 'L', 'N']
     weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-
     model = cp_model.CpModel()
 
     with open('resources/datasets/n005w4/Sc-n005w4.json') as file:
@@ -51,6 +52,12 @@ all_working_shifts = range(len(shiftTypes))
 all_shifts = range(len(shiftTypes) + 1)
 all_days = range(len(weekDays) * len(planningHorizon))
 all_skills = range(len(skills))
+
+
+shift_sequence_constraints = [(1, 2, 2,  15, 5, 5, 15), (2, 2, 2, 15, 3, 3, 15), (3, 4, 4, 15, 5, 5, 15)]
+day_off_sequence_constraints = [(2, 2, 30, 3, 3, 30), (3, 3, 30, 5, 5, 30)]
+working_sequence_constraints = [(3, 3, 30, 5, 5, 30)]
+
 
 def create_forbidden_succession(scData):
     forbidden_array_str = []
@@ -148,11 +155,93 @@ def create_request(wData, nurseList, shiftList, dayList):
             requests.append(request)
     return requests
 
+def negated_bounded_span(works, start, length):
+    """Filters an isolated sub-sequence of variables assined to True.
+  Extract the span of Boolean variables [start, start + length), negate them,
+  and if there is variables to the left/right of this span, surround the span by
+  them in non negated form.
+  Args:
+    works: a list of variables to extract the span from.
+    start: the start to the span.
+    length: the length of the span.
+  Returns:
+    a list of variables which conjunction will be false if the sub-list is
+    assigned to True, and correctly bounded by variables assigned to False,
+    or by the start or end of works.
+  """
+    sequence = []
+    # Left border (start of works, or works[start - 1])
+    if start > 0:
+        sequence.append(works[start - 1])
+    for i in range(length):
+        sequence.append(works[start + i].Not())
+    # Right border (end of works or works[start + length])
+    if start + length < len(works):
+        sequence.append(works[start + length])
+    return sequence
+
+
+def add_soft_sequence_constraint(model, works, hard_min, soft_min, min_cost,
+                                 soft_max, hard_max, max_cost, prefix):
+    """Sequence constraint on true variables with soft and hard bounds.
+  This constraint look at every maximal contiguous sequence of variables
+  assigned to true. If forbids sequence of length < hard_min or > hard_max.
+  Then it creates penalty terms if the length is < soft_min or > soft_max.
+  Args:
+    model: the sequence constraint is built on this model.
+    works: a list of Boolean variables.
+    hard_min: any sequence of true variables must have a length of at least
+      hard_min.
+    soft_min: any sequence should have a length of at least soft_min, or a
+      linear penalty on the delta will be added to the objective.
+    min_cost: the coefficient of the linear penalty if the length is less than
+      soft_min.
+    soft_max: any sequence should have a length of at most soft_max, or a linear
+      penalty on the delta will be added to the objective.
+    hard_max: any sequence of true variables must have a length of at most
+      hard_max.
+    max_cost: the coefficient of the linear penalty if the length is more than
+      soft_max.
+    prefix: a base name for penalty literals.
+  Returns:
+    a tuple (variables_list, coefficient_list) containing the different
+    penalties created by the sequence constraint.
+  """
+    cost_literals = []
+    cost_coefficients = []
+
+    # Penalize sequences that are below the soft limit.
+    if min_cost > 0:
+        for length in range(hard_min, soft_min):
+            for start in range(len(works) - length + 1):
+                span = negated_bounded_span(works, start, length)
+                name = ': under_span(start=%i, length=%i)' % (start, length)
+                lit = model.NewBoolVar(prefix + name)
+                span.append(lit)
+                model.AddBoolOr(span)
+                cost_literals.append(lit)
+                # We filter exactly the sequence with a short length.
+                # The penalty is proportional to the delta with soft_min.
+                cost_coefficients.append(min_cost * (soft_min - length))
+
+    # Penalize sequences that are above the soft limit.
+    if max_cost > 0:
+        for length in range(soft_max + 1, hard_max + 1):
+            for start in range(len(works) - length + 1):
+                span = negated_bounded_span(works, start, length)
+                name = ': over_span(start=%i, length=%i)' % (start, length)
+                lit = model.NewBoolVar(prefix + name)
+                span.append(lit)
+                model.AddBoolOr(span)
+                cost_literals.append(lit)
+                # Cost paid is max_cost * excess length.
+                cost_coefficients.append(max_cost * (length - soft_max))
+
+    return cost_literals, cost_coefficients
+
 weekly_demand = create_weekly_demand(planningHorizon, weekDays)
 requests = create_request(planningHorizon, nurses, shiftTypes, weekDays)
 forbidden_shift_succession = create_forbidden_succession(scenarioDict)
-
-print(weekly_demand)
 
 obj_int_vars = []
 obj_int_coeffs = []
@@ -171,9 +260,15 @@ for n in all_nurses:
     for d in all_days:
         model.AddExactlyOne((shifts[n, d, s] for s in all_shifts))
 
+# H3: Forbidden shift successions
+for previous_shift, next_shift, cost in forbidden_shift_succession:
+    for n in all_nurses:
+        for d in range((len(all_days) - 1)):
+            transition = [shifts[n, d, previous_shift].Not(), shifts[n, d + 1, next_shift].Not()]
+            model.AddBoolOr(transition)
+
 # H2 H4 S1: Each shift is assigned to opt or min requirements of nurses depending on their skills.
 for s in range(1, len(shiftTypes) + 1):
-    print(s)
     for d in all_days:
         for f in range(len(skills)):
             works = [shifts[n, d, s] for n in find_indices(nurses, lambda n: skills[f] in n.skills)]
@@ -186,16 +281,36 @@ for s in range(1, len(shiftTypes) + 1):
         obj_int_vars.append(insufficient)
         obj_int_coeffs.append(30)
 
-
-
-# H3: Forbidden shift successions
-for previous_shift, next_shift, cost in forbidden_shift_succession:
+# S2: Consecutive Assignment 1/2
+#TODO: Implement consecutive working days between different shifttype (e.g. EEELLL)
+for ct in shift_sequence_constraints:
+    shift, hard_min, soft_min, min_cost, hard_max, soft_max, max_cost = ct
     for n in all_nurses:
-        for d in range((len(all_days) - 1)):
-            transition = [shifts[n, d, previous_shift].Not(), shifts[n, d + 1, next_shift].Not()]
-            model.AddBoolOr(transition)
+        works = [shifts[n, d, shift] for d in all_days]
+        variables, coeffs = add_soft_sequence_constraint(
+            model, works, 1, soft_min, min_cost, soft_max, len(all_days), max_cost, 'shift_constraint(employee %i, shift %i)' % (n, shift))
+        obj_bool_vars.extend(variables)
+        obj_bool_coeffs.extend(coeffs)
 
-test = [1,2,3]
+# S3: Consecutive Days Off
+for c in range(len(contracts)):
+    cn = find_indices(nurses, lambda n: contracts[c].id == n.contract.id)
+    for n in range(len(cn)):
+        hard_min, soft_min, min_cost, hard_max, soft_max, max_cost = day_off_sequence_constraints[c]
+        works = [shifts[cn[n], d, 0] for d in all_days]
+        variables, coeffs = add_soft_sequence_constraint(
+            model, works, 1, soft_min, min_cost, soft_max, len(all_days), max_cost, 'day_off_sequence_constraint(employee %i, shift %i)' % (n, shift))
+        obj_bool_vars.extend(variables)
+        obj_bool_coeffs.extend(coeffs)
+
+# S4: Preferences
+for n, s, d, w in requests:
+    preference = [shifts[n, d, s]]
+    pref_var = model.NewBoolVar('preference (employee=%i, day=%i, shift=%i)' % (n, d, s))
+    preference.append(pref_var)
+    model.AddBoolOr(preference)
+    obj_bool_vars.append(pref_var)
+    obj_bool_coeffs.append(w)
 
 # S5: Complete weekends
 #TODO: Check why same shifts on weekends are alwasys choosen
@@ -207,7 +322,6 @@ for n in all_nurses:
                 sat_weekends = [shifts[n, d + (w * len(weekDays)), 0].Not(), shifts[n, d + (w * len(weekDays)) + 1, 0]]
                 sun_week_var = model.NewBoolVar('complete weekend (employee=%i, week=%i)' % (n, w))
                 sat_week_var = model.NewBoolVar('complete weekend (employee=%i, week=%i)' % (n, w))
-                print(sun_weekends)
                 sun_weekends.append(sun_week_var)
                 sat_weekends.append(sat_week_var)
                 model.AddBoolXOr(sun_weekends)
@@ -217,19 +331,6 @@ for n in all_nurses:
                 obj_bool_vars.append(sat_week_var)
                 obj_bool_coeffs.append(30)
 
-
-# S4: Preferences
-for n, s, d, w in requests:
-    preference = [shifts[n, d, s]]
-    pref_var = model.NewBoolVar('preference (employee=%i, day=%i, shift=%i)' % (n, d, s))
-    preference.append(pref_var)
-    model.AddBoolOr(preference)
-    obj_bool_vars.append(pref_var)
-    obj_bool_coeffs.append(w)
-
-print(obj_bool_vars)
-print(obj_int_vars)
-print(obj_int_coeffs)
 
 model.Minimize(
     sum(obj_bool_vars[i] * obj_bool_coeffs[i]
